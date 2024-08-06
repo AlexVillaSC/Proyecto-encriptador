ab71cd821a3c2d12f13e6cc58965f672d69d99e4
alexander <alexvillafuertesantacruz@gmail.com>
2024-08-05 21:43:22 -0500

    Avanze de javascript

--- logica.js
+++ logica.js
@@ -1,5 +1,15 @@
 
 
+let entrada_texto = document.querySelector('textarea');
+
+let texto_encriptado = document.getElementById('texto-encriptado')
+
+
+function encriptar () {
+    texto_encriptado.innerHTML = entrada_texto.value;
+}
+
+
 
 
 
